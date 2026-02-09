53374c17878e378226470eb4f1eccc6c715f7256
Old <225056366+roy-henderson@users.noreply.github.com>
2026-02-09 15:05:09 -0500

    feat: Embed CRAudioVizAI JavariVerse experience in iframe

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 export default function Home() {
   return (
-    <div style={{ width: '100%', height: '100vh', margin: 0, padding: 0 }}>
-      <iframe
-        src="https://craudiovizai.com/javari-verse"
-        style={{
-          border: 'none',
-          width: '100%',
-          height: '100%',
-        }}
-        title="JavariVerse"
-      />
-    </div>
+    <iframe
+      src="https://craudiovizai.com/javari-verse"
+      style={{
+        border: "none",
+        width: "100%",
+        height: "100vh",
+      }}
+    />
   );
 }
